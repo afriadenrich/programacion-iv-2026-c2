5062d228ab974d877a8cae1826e803cc806a8987
AgustinFrich <agustinfrich@gmail.com>
2026-08-20 21:49:32 -0300

    Clase 02: Enlaces y signals

--- clase-02-ruteo-bindeos-componentes/src/app/pages/tutoriales/tutoriales.ts
+++ clase-02-ruteo-bindeos-componentes/src/app/pages/tutoriales/tutoriales.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
+import { Card } from './components/card/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [Card, FormsModule],
   selector: 'app-tutoriales',
   styleUrl: './tutoriales.css',
   templateUrl: './tutoriales.html',
 })
-export class Tutoriales {}
+export class Tutoriales {
+  // signal / señal. La señal va a indiar que se debe renderizar de vuelta la pantalla cuando sufra algún cambio.
+  contador: WritableSignal<number> = signal(0);
+
+  nombre = 'asd';
+
+  imagen: string = 'img/158.png';
+
+  contar(): void {
+    // Así obtengo su valor
+    console.log(this.contador());
+
+    // Así le cambio el valor
+    // this.contador.set(this.contador() + 1);
+    this.contador.update((prev) => ++prev);
+  }
+
+  reiniciar() {
+    this.nombre = '';
+  }
+
+  intervalo?: number;
+
+  ngOnInit() {
+    this.intervalo = setInterval(() => {
+      this.contar();
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    clearInterval(this.intervalo);
+  }
+}
