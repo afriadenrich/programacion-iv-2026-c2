a8ed2ee828a8838688ff535e4197766c0a435fd7
AgustinFrich <agustinfrich@gmail.com>
2026-09-01 21:28:52 -0300

    Clase 05:  asdas

--- clase-05-auth/src/app/app.routes.ts
+++ clase-05-auth/src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
     // canActivateChild: [logueadoGuard],
+    // Debería agregar un canActivate para no poder entrar si estoy logueado
   },
   {
     path: 'home',
