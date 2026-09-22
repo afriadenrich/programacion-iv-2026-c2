eb47b93667afef6499d27ad0209a4725bfb5f0a7
AgustinFrich <agustinfrich@gmail.com>
2026-09-22 19:57:54 -0300

    Clase 10: Ahora registro las notificaciones en supabase

--- clase-10-notificaciones/src/app/app.ts
+++ clase-10-notificaciones/src/app/app.ts
@@ -25,6 +25,18 @@ export class App {
 
     const json = subscription.toJSON();
 
-    console.log(json);
+    const endpoint = subscription.endpoint;
+    const auth = subscription.getKey('auth');
+    const p256dh = subscription.getKey('p256dh');
+
+    const { data, error } = await this.supabaseService.From('Suscripciones_Notificaciones').insert({
+      endpoint: json.endpoint,
+      auth: json.keys?.['auth'],
+      p256dh: json.keys?.['p256dh'],
+    });
+
+    if (error) {
+      console.log('falló la subida a supabase, ver cómo lo solucionamos jaja');
+    }
   }
 }
